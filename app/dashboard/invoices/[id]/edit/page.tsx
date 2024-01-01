3f1cf7f41bdaa0f7fb2e5e958edde9a2324d516c
yken88 <yken1019@icloud.com>
2024-01-01 10:54:57 +0900

    add invoices

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from '@/app/ui/invoices/edit-form';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    }
+                ]}
+            />
+            <Form invoice={invoice} customers={customers}/>
+        </main>
+    )
+}
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-    return <p>Invoices Page</p>;
+// import Pagination form "@/app/ui/invoices/pagination";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { Suspense } from "react";
+import InvoicesTableSkelton from "@/app/ui/skeletons";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    }
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page || 1);
+    const totalPages = await fetchInvoicesPages(query);
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2x1`}>Invoices</h1>
+            </div>
+
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..."/>
+            </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkelton />}>
+                <Table query={query} currentPage={currentPage}/>
+            </Suspense>
+
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages}/>
+            </div>
+        </div> 
+    )
 }
